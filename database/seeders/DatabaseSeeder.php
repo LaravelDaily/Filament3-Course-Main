e19588ac42a737f9e3683d239a9f9d415706736c
modestasv <info@modestasv.lt>
2023-08-17 15:30:04 +0300

    Fixing fresh database migrations issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(UserSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(TagSeeder::class);
         $this->call(ProductSeeder::class);
